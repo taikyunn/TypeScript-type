89b4807e61aa4c6a00f9c57f4ffd540f1c4063f4
taikyunn <taichi_141@me.com>
2022-03-26 11:36:05 +0900

    noImplicitReturns
    returnを明示的に行なっていない場合にエラーを出力させるもの。

--- additionalCheck.ts
+++ additionalCheck.ts
@@ -7,3 +7,10 @@ function noUnusedLocals() {
 function noUnusedParameters(num1: number) {
   console.log(num1);
 }
+
+function noImplicitReturns(message: string) :string | undefined {
+  if (message) {
+    return message;
+  }
+  return;
+}
