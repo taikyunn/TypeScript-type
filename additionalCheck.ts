fc14a115adeca972217db49b70986e4f77424da5
taikyunn <taichi_141@me.com>
2022-03-26 11:32:42 +0900

    noUnusedLocals
    ローカル変数が使用されていない場合に、エラーを出力する設定。

---
+++ additionalCheck.ts
@@ -0,0 +1,9 @@
+let global = "globalHello";
+function noUnusedLocals() {
+  let noUnusedLocals = 'test';
+  console.log(noUnusedLocals)
+}
+
+function noUnusedParameters(num1: number) {
+  console.log(num1);
+}
---
+++ dist/additionalCheck.js
@@ -0,0 +1,9 @@
+"use strict";
+let global = "globalHello";
+function noUnusedLocals() {
+    let noUnusedLocals = 'test';
+    console.log(noUnusedLocals);
+}
+function noUnusedParameters(num1) {
+    console.log(num1);
+}
